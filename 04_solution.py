0617d1d8d620238ee75926958988eefe8a8272a7
Mayank saini <95845450+Mayanks08@users.noreply.github.com>
2024-05-26 16:34:15 +0530

    Basic code files

---
+++ 04_solution.py
@@ -0,0 +1,14 @@
+fruit = "Banana"
+color = "green"
+
+if fruit == "Banana":
+    if color == "green":
+        print("Unripe")
+    elif color == "yellow":
+        print("Ripe")
+    else :
+        print("overripe")
+
+
+if fruit != 'Banana':
+    print("please take valid fruit")
---
+++ 05_solution.py
@@ -0,0 +1,11 @@
+weather = "rainy"
+
+if weather =="sunny":
+    activity= "It's a beautiful day! Go outside and enjoy the sunshine!"
+elif weather == "rainy":
+    activity= "It's a rainy day. Stay inside and have a cup of tea."
+elif weather == "snowy":
+    activity = "lets build snownman at Backyard"
+
+
+print(activity)
