da4b9141155dbe99dabcf42ee1f17bbb113fe055
Mayank Saini <95845450+Mayanks08@users.noreply.github.com>
2025-09-04 13:45:55 +0530

    Update rag1.py

--- rag1.py
+++ rag1.py
@@ -6,9 +6,14 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+import os
 
-
+load_dotenv()
 pdf_path = Path(__file__).parent/ "C++_notes.pdf" 
+client =OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 loader = PyPDFLoader(
     file_path=pdf_path,
@@ -18,12 +23,12 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 split_docs= text_splitter.split_documents(documents=docs)
 embedder =OpenAIEmbeddings(
     model="text-embedding-3-large",
-    api_key=apikey
+    
 )
 
 # vector_store=QdrantVectorStore.from_documents(
 #     documents=split_docs,
-#     collection_name="learning_langchain",
+#     collection_name="learning C++",
 #     url="http://localhost:6333",
 #     embedding=embedder
 # )
@@ -32,10 +37,52 @@ embedder =OpenAIEmbeddings(
 print("Injection Done")
 
 retriver =QdrantVectorStore.from_existing_collection(
-     collection_name="learning_langchain",
+     collection_name="learning C++",
      url="http://localhost:6333",
      embedding=embedder
 )
 
-search_result = retriver.similarity_search(query="what is Pointer in c++?")
-print("Relevant chunks",search_result)
+
+# while True:
+#     query = input("\nAsk me something about C++ (or type 'exit' to quit): ")
+
+#     if query.lower() in ["exit", "quit"]:
+#         print("Goodbye!")
+#         break
+
+#    
+#     search_result = retriver.similarity_search(query=query, k=3)
+
+#     if not search_result:
+#         print(" No relevant chunks found.")
+#         continue
+
+#     # print chunks
+#     for i, doc in enumerate(search_result):
+#         page_number = doc.metadata.get("page", "Unknown")
+#         print(f"\n--- Relevant Chunk {i+1} ---")
+#         print("Content:\n", doc.page_content.strip())
+#         print(f"Page Number: {page_number}")
+
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",   
+    temperature=0 ,        
+    
+)
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    retriever=retriver.as_retriever(),
+    chain_type="stuff",  
+)
+
+while True:
+    query = input("\nAsk me something about C++ (or type 'exit' to quit): ")
+
+    if query.lower() in ["exit", "quit"]:
+        print("Goodbye!")
+        break
+
+
+    answer = qa_chain.invoke(query)
+    print("\n💡 Structured Answer:\n", answer)
